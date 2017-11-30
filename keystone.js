87030de259d1b002817ed96795d2e517f12b87d8
Justin Donnaruma <Justin@Donnaruma.us>
2017-11-30 14:08:33 -0500

    add new relic monitoring

--- keystone.js
+++ keystone.js
@@ -1,7 +1,7 @@
 // Simulate config options from your production environment by
 // customising the .env file in your project's root folder.
 require('dotenv').config();
-
+require('newrelic');
 // Require keystone
 var keystone = require('keystone');
 var handlebars = require('express-handlebars');
